c5e1fc60e76e1d276b04557cdbfae6ee8bd9d59d
Marco Janssen <marco.janssen@outlook.com>
2013-07-17 20:28:28 +0200

    Implemented sqlite request

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
+ini_set('display_errors',1);
+
+use Symfony\Component\HttpFoundation\Request;
 
 $app = new Silex\Application();
 $app['debug'] = true;
@@ -11,15 +14,21 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     ),
 ));
 
-$app->get('/', function () {
+$app->get('/', function (Request $request) use ($app) {
 
-    $items['items'] = array();
-    for($i=0; $i<25; $i++) {
-        $items['items'][] = array(
-            'id'   => $i,
-            'name' => 'w00t'
-        );
+    $limit = $request->query->get('limit');
+    $start = $request->query->get('start');
+
+    if(empty($limit)) {
+        $limit = 25;
     }
+
+    if(empty($start)) {
+        $start = 0;
+    }
+
+    $sql = "SELECT * FROM items LIMIT ?,?";
+    $items = $app['db']->fetchAll($sql, array((int) $start, (int) $limit));
 
     return json_encode($items);
 });
