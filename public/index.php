4ce2a180d7c41d51bd90f58e318f9502d87ef95a
Marco Janssen <marco.janssen@outlook.com>
2013-07-17 20:11:28 +0200

    Added sqlite & doctrine dbal

--- public/index.php
+++ public/index.php
@@ -2,7 +2,26 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
+$app['debug'] = true;
 
-// definitions
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => 'pdo_sqlite',
+        'path'     => __DIR__.'/../db/app.db',
+    ),
+));
+
+$app->get('/', function () {
+
+    $items['items'] = array();
+    for($i=0; $i<25; $i++) {
+        $items['items'][] = array(
+            'id'   => $i,
+            'name' => 'w00t'
+        );
+    }
+
+    return json_encode($items);
+});
 
 $app->run();
