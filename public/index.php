3491c19fce258319026e6366e2349be94258666e
Marco Janssen <marco.janssen@outlook.com>
2013-09-14 07:25:41 +0200

    Added DoctrineORM service provider

--- public/index.php
+++ public/index.php
@@ -3,16 +3,33 @@ require_once __DIR__.'/../vendor/autoload.php';
 ini_set('display_errors',1);
 
 use Symfony\Component\HttpFoundation\Request;
+use Silex\Provider\DoctrineServiceProvider;
+use Dflydev\Silex\Provider\DoctrineOrm\DoctrineOrmServiceProvider;
+
 
 $app = new Silex\Application();
 $app['debug'] = true;
 
-$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+$app->register(new DoctrineServiceProvider(), array(
     'db.options' => array(
         'driver'   => 'pdo_sqlite',
         'path'     => __DIR__.'/../db/app.db',
     ),
 ));
+
+$app->register(new DoctrineOrmServiceProvider, array(
+    "orm.proxies_dir" => __DIR__."/../src/MJ/Doctrine/Proxies",
+    "orm.em.options" => array(
+        "mappings" => array(
+            array(
+                "type" => "annotation",
+                "namespace" => "MJ\Doctrine\Entities",
+                "resources_namespace" => "MJ\Doctrine\Entities",
+            )
+        ),
+    ),
+));
+
 
 $app->get('/', function (Request $request) use ($app) {
 
