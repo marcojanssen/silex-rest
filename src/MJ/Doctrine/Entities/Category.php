b3ea08d6d94f6cbc70cc892e7198d6d5ce8b254b
Marco Janssen <marco.janssen@outlook.com>
2013-09-14 22:02:53 +0200

    Added category entity

---
+++ src/MJ/Doctrine/Entities/Category.php
@@ -0,0 +1,48 @@
+<?php
+namespace MJ\Doctrine\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="category")
+ */
+class Category
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+}
