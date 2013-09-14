ae40e1976a6f06a318cd8342e8491bfe82ebfc15
Marco Janssen <marco.janssen@outlook.com>
2013-09-14 07:41:19 +0200

    Implemented silex skeleton

---
+++ app/config/dev.php
@@ -0,0 +1,19 @@
+<?php
+
+use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\WebProfilerServiceProvider;
+
+// include the prod configuration
+require __DIR__ . '/prod.php';
+
+// enable the debug mode
+$app['debug'] = true;
+
+$app->register(new MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__.'/../logs/silex_dev.log',
+));
+
+$app->register($p = new WebProfilerServiceProvider(), array(
+    'profiler.cache_dir' => __DIR__.'/../cache/profiler',
+));
+$app->mount('/_profiler', $p);
