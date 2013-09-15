4b2780f5fcb9325e266a3fab8ae3928152320de9
Marco Janssen <marco.janssen@outlook.com>
2013-09-15 14:47:54 +0200

    Renamed & moved controllers.php to app/routes.php

--- public/index.php
+++ public/index.php
@@ -3,5 +3,5 @@ $loader = require_once __DIR__.'/../vendor/autoload.php';
 
 $app = require __DIR__.'/../app/app.php';
 require __DIR__.'/../app/config/prod.php';
-require __DIR__.'/../src/controllers.php';
+require __DIR__.'/../app/routes.php';
 $app->run();
--- public/index_dev.php
+++ public/index_dev.php
@@ -14,5 +14,5 @@ if ('cli' !== php_sapi_name()) {
 
 $app = require __DIR__.'/../app/app.php';
 require __DIR__.'/../app/config/dev.php';
-require __DIR__.'/../src/controllers.php';
+require __DIR__.'/../app/routes.php';
 $app->run();
