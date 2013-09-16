3894cfa6b5ac4008bcf2a830dc63a9a7ff798eb5
Marco Janssen <marco.janssen@outlook.com>
2013-09-16 23:20:56 +0200

    Forced secure on index

--- public/index.php
+++ public/index.php
@@ -2,6 +2,9 @@
 $loader = require_once __DIR__.'/../vendor/autoload.php';
 
 $app = require __DIR__.'/../app/app.php';
+
+$app['controllers']->requireHttps();
+
 require __DIR__.'/../app/config/prod.php';
 require __DIR__.'/../app/routes.php';
 $app->run();
