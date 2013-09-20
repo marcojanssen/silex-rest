992d39c9a683eca8d9d12f13d9130638407bdce9
Marco Janssen <marco.janssen@outlook.com>
2013-09-20 21:37:42 +0200

    Removed web profiler & twig

--- public/index_dev.php
+++ public/index_dev.php
@@ -3,7 +3,6 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 use Herrera\Wise\WiseServiceProvider;
 use Silex\Application;
 use Silex\Provider\MonologServiceProvider;
-use Silex\Provider\WebProfilerServiceProvider;
 use Symfony\Component\Debug\Debug;
 
 chdir(dirname(__DIR__));
@@ -52,17 +51,8 @@ $app->register(new MonologServiceProvider(), array(
     'monolog.logfile' => $app['config']['monolog']['logfile'],
 ));
 
-$app->register($p = new WebProfilerServiceProvider(), array(
-    'profiler.cache_dir' => $app['config']['profiler']['cache_dir'],
-));
-
 $app->register(new Whoops\Provider\Silex\WhoopsServiceProvider());
 
-$app->mount($app['config']['base.url'].'/_profiler', $p);
 $app->mount($app['config']['base.url'].'/{namespace}', new MJanssen\Provider\RestControllerProvider());
-
-$app->error(function (\Exception $e, $code) use ($app) {
-    return;
-});
 
 $app->run();
