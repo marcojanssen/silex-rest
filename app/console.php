237b5dc2545643701fb7e9d7289585e96445343f
Marco Janssen <marco.janssen@outlook.com>
2013-09-26 21:45:38 +0200

    Added basic commands for clearing cache/logs

--- app/console.php
+++ app/console.php
@@ -9,20 +9,44 @@ use Symfony\Component\Console\Helper\HelperSet;
 use Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper;
 use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 
 
-$console = new Application('MJanssen/Silex', 'n/a');
+$console = new Application('Silex - Rest API Edition', '1.0');
 $console->getDefinition()->addOption(new InputOption('--env', '-e', InputOption::VALUE_REQUIRED, 'The Environment name.', 'dev'));
-$console
-    ->register('my-command')
-    ->setDefinition(array(
-        // new InputOption('some-option', null, InputOption::VALUE_NONE, 'Some help'),
-    ))
-    ->setDescription('My command description')
-    ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
-        // do something
-    })
-;
+
+if (isset($app['cache.path'])) {
+    $console
+        ->register('cache:clear')
+        ->setDescription('Clears the cache')
+        ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
+
+            $cacheDir = $app['cache.path'];
+            $finder = Finder::create()->in($cacheDir)->notName('.gitkeep');
+
+            $filesystem = new Filesystem();
+            $filesystem->remove($finder);
+
+            $output->writeln(sprintf("%s <info>success</info>", 'cache:clear'));
+        });
+}
+
+if (isset($app['log.path'])) {
+    $console
+        ->register('log:clear')
+        ->setDescription('Clears the logs')
+        ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
+
+            $cacheDir = $app['log.path'];
+            $finder = Finder::create()->in($cacheDir)->notName('.gitkeep');
+
+            $filesystem = new Filesystem();
+            $filesystem->remove($finder);
+
+            $output->writeln(sprintf("%s <info>success</info>", 'log:clear'));
+        });
+}
 
 /*
  * Doctrine CLI
