8f585c1b4f9a0568925a722280c3f9ef3664a3c7
Marco Janssen <marco.janssen@outlook.com>
2013-09-27 23:49:34 +0200

    Switched config to php

---
+++ app/config/config.php
@@ -0,0 +1,50 @@
+<?php
+return array(
+    //application settings
+    'baseUrl' => '',
+
+    //db settings
+    'db.options' => array(
+        'driver' => 'pdo_mysql',
+        'charset' => 'UTF8',
+        'master' => array(
+            'user'     => 'root',
+            'password' => 'root',
+            'host'     => 'localhost',
+            'dbname'   => 'silexrest'
+        ),
+        'slaves' => array(
+            array(
+                'user'     => 'root',
+                'password' => 'root',
+                'host'     => 'localhost',
+                'dbname'   => 'silexrest'
+            )
+        ),
+        'wrapperClass' => 'Doctrine\DBAL\Connections\MasterSlaveConnection'
+    ),
+
+    'orm.proxies_dir'   => '%app_path%/app/cache/Doctrine/Proxies',
+    'orm.default_cache' => 'apc', #should be set to apc/xcache on production
+    'orm.em.options'    => array(
+        'mappings' => array(
+            array(
+                'type'      => 'annotation',
+                'namespace' => 'MJanssen\Doctrine\Entity',
+                'alias'     => 'core',
+                'path'      => '%app_path%/src/MJanssen/Doctrine/Entity',
+                'use_simple_annotation_reader' => false
+            )
+        )
+    ),
+
+    //paths for cache & logs
+    'app_path'   => '%app_path%',
+    'cache.path' => '%app_path%/app/cache',
+    'log.path'   => '%app_path%/app/logs',
+
+    'baseUrl'	 => '',
+    'hmac_key'	 => '',
+
+    'serializer.cache.dir' => '%app_path%/app/cache/serializer'
+);
