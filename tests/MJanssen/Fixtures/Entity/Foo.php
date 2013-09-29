f226622f3a9d31fdde87736fec1e09a8448c511b
Marco Janssen <marco.janssen@outlook.com>
2013-09-29 20:14:03 +0200

    Added ExtractorServiceTest

---
+++ tests/MJanssen/Fixtures/Entity/Foo.php
@@ -0,0 +1,34 @@
+<?php
+namespace Mjanssen\Fixtures\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation\Type;
+use JMS\Serializer\Annotation\Groups;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="foo")
+ */
+class Foo
+{
+    /**
+     * @Type("integer")
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     * @Groups({"foo"})
+     */
+    protected $id;
+
+    /**
+     * @Type("string")
+     * @ORM\Column(type="string")
+     * @Groups({"foo"})
+     */
+    protected $name;
+
+    public function __set($name, $value)
+    {
+        $this->{$name} = $value;
+    }
+}
