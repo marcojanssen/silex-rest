80cc95da91810106e31faf8004b8a94c0a598cc6
Marco Janssen <marco.janssen@outlook.com>
2013-10-20 10:03:32 +0200

    Added unit tests for get & getcollection methods

---
+++ tests/MJanssen/Service/RestEntityServiceTest.php
@@ -0,0 +1,147 @@
+<?php
+namespace MJanssen\Service;
+
+use Silex\Application;
+use MJanssen\Fixtures\Entity\Test;
+
+class RestEntityServiceTest extends \PHPUnit_Framework_TestCase
+{
+    protected $getOutput = array('id' => 1, 'name' => 'foobaz');
+
+    /**
+     * Test get action
+     */
+    public function testGetAction()
+    {
+        $service = $this->getService();
+        $response = $service->getAction(1);
+
+        $this->assertEquals(
+            $response->getContent(),
+            json_encode($this->getOutput)
+        );
+    }
+
+    /**
+     * Test get collection action
+     */
+    public function testGetCollectionAction()
+    {
+        $service = $this->getService();
+        $response = $service->getCollectionAction(1);
+
+        $this->assertEquals(
+            $response->getContent(),
+            json_encode(
+                array($this->getOutput, $this->getOutput)
+            )
+        );
+    }
+
+    /**
+     * @return RestEntityService
+     */
+    protected function getService()
+    {
+        return new RestEntityService(
+            $this->getMockRequest(),
+            $this->getMockApplication()
+        );
+
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getMockRequest()
+    {
+        return $this->getMock('Symfony\Component\HttpFoundation\Request', array('getContent'));
+    }
+
+    /**
+     * Get a mock silex service
+     * @return Application
+     */
+    protected function getMockApplication()
+    {
+        $app = new Application();
+
+        $em = $this->getMock('\Doctrine\ORM\EntityManager', array('getRepository'), array(), '', false);
+
+        $em->expects($this->any())
+           ->method('getRepository')
+           ->will($this->returnValue($this->getEntityRepository()));
+
+        $app['orm.em'] = $em;
+
+        $app['doctrine.resolver'] = $this->getResolverServiceMock();
+        $app['doctrine.extractor'] = $this->getExtractorServiceMock();
+
+        return $app;
+    }
+
+    /**
+     * Create a mock entity
+     * @param $args
+     * @return Foo
+     */
+    protected function createEntity($args)
+    {
+        $entity = new Test;
+        $entity->id = $args['id'];
+        $entity->name = $args['name'];
+        return $entity;
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getEntityRepository()
+    {
+        $entity = $this->createEntity(array('id' => 1, 'name' => 'foobaz'));
+
+        $repository = $this->getMock('\Doctrine\ORM\EntityRepository', array('findOneBy', 'findBy'), array(), '', false);
+
+        $repository->expects($this->any())
+                   ->method('findOneBy')
+                   ->will($this->returnValue($entity));
+
+        $repository->expects($this->any())
+                   ->method('findBy')
+                   ->will($this->returnValue(array($entity,$entity)));
+
+        return $repository;
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getResolverServiceMock()
+    {
+        $service = $this->getMock('MJanssen\Service\ResolverService', array('getEntityClassName'), array(), '', false);
+
+        $service->expects($this->any())
+                ->method('getEntityClassName')
+                ->will($this->returnValue('MJanssen\Fixtures\Entity\Test'));
+
+        return $service;
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getExtractorServiceMock()
+    {
+        $service = $this->getMock('MJanssen\Service\ExtractorService', array('extractEntity', 'extractEntities'), array(), '', false);
+
+        $service->expects($this->any())
+                ->method('extractEntity')
+                ->will($this->returnValue($this->getOutput));
+
+        $service->expects($this->any())
+                ->method('extractEntities')
+                ->will($this->returnValue(array($this->getOutput, $this->getOutput)));
+
+        return $service;
+    }
+}
