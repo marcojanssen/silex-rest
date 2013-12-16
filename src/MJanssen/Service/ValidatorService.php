0ac521dd147efdbecffb2f35cb96333c6fd20aff
Marco Janssen <dev@marcojanssen.net>
2013-12-16 21:03:51 +0100

    Removed silex error trait package

--- src/MJanssen/Service/ValidatorService.php
+++ src/MJanssen/Service/ValidatorService.php
@@ -2,11 +2,11 @@
 namespace MJanssen\Service;
 
 use Symfony\Component\Validator\Validator;
-use Wizkunde\Traits\ErrorTrait;
+use Symfony\Component\Validator\ConstraintViolationList;
 
 class ValidatorService
 {
-    use ErrorTrait;
+    protected $errors = array();
 
     /**
      * @var \Symfony\Component\Validator\Validator
@@ -65,6 +65,58 @@ class ValidatorService
                 $this->getValidatorConstrainClass()->getConstraints()
             )
         );
+    }
+
+    /**
+     * Check if errors exist
+     * @return bool
+     */
+    public function hasErrors()
+    {
+        if (count($this->errors) > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Returns the errors
+     *
+     * @return array
+     */
+    public function getErrors()
+    {
+        if ($this->hasErrors()) {
+            return $this->errors;
+        } else {
+            return;
+        }
+    }
+
+    /**
+     * @param array $errors
+     *
+     * @throws \Exception
+     */
+    public function setErrors(ConstraintViolationList $errors)
+    {
+        $this->errors = $errors;
+    }
+
+    /**
+     * Return the errors in a response ready format
+     *
+     * @return array with errors
+     */
+    public function getErrorResponse()
+    {
+        $errorResponse = array();
+
+        foreach ($this->getErrors() as $error) {
+            $errorResponse[] = $error->getPropertyPath().' '.$error->getMessage()."\n";
+        }
+
+        return $errorResponse;
     }
 
     /**
