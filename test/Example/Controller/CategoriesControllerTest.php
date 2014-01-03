b765921a606e9435b4912f373f11047b70b1e9d4
Marco Janssen <dev@marcojanssen.net>
2014-01-03 10:56:13 +0100

    Updated controller tests

--- test/Example/Controller/CategoriesControllerTest.php
+++ test/Example/Controller/CategoriesControllerTest.php
@@ -6,8 +6,63 @@ namespace Example\Controller;
  * Class CategoriesControllerTest
  * @package Example\Controller
  */
-class CategoriesControllerTest extends \MJanssen\Controller\ControllerTest
+class CategoriesControllerTest extends \MJanssen\Controller\AbstractControllerTest
 {
+    /**
+     * Test if the get action works
+     */
+    public function testGetAction()
+    {
+        $this->assertInstanceOf('Symfony\Component\HttpFoundation\Response', $this->doGetAction());
+    }
+
+    /**
+     * Test if the getCollection action works
+     */
+    public function testGetCollectionAction()
+    {
+        $this->assertInstanceOf('Symfony\Component\HttpFoundation\Response', $this->doGetCollectionAction());
+    }
+
+    /**
+     * Test if the delete action works
+     */
+    public function testDeleteAction()
+    {
+        $this->assertInstanceOf('Symfony\Component\HttpFoundation\Response', $this->doDeleteAction());
+    }
+
+    /**
+     * Test if the put action works
+     */
+    public function testPutAction()
+    {
+        $this->assertInstanceOf('Symfony\Component\HttpFoundation\Response', $this->doPutAction());
+    }
+
+    /**
+     * Test if the post action works
+     */
+    public function testPostAction()
+    {
+        $this->assertInstanceOf('Symfony\Component\HttpFoundation\Response', $this->doPostAction());
+    }
+
+    /**
+     * Test if every method returns a response
+     */
+    public function testResolveAction()
+    {
+        $this->assertInstanceOf('Symfony\Component\HttpFoundation\Response', $this->doResolveAction('GET'));
+        $this->assertInstanceOf('Symfony\Component\HttpFoundation\Response', $this->doResolveAction('GET', 1));
+        $this->assertInstanceOf('Symfony\Component\HttpFoundation\Response', $this->doResolveAction('POST'));
+        $this->assertInstanceOf('Symfony\Component\HttpFoundation\Response', $this->doResolveAction('PUT'));
+        $this->assertInstanceOf('Symfony\Component\HttpFoundation\Response', $this->doResolveAction('DELETE'));
+    }
+
+    /**
+     * @return CategoriesController
+     */
     protected function getTestController()
     {
         return new CategoriesController();
